7df9f19659cb32f62ba95e9d5a2d44df10c0fb47
Luke <lucasromerodb@gmail.com>
2025-03-02 21:34:59 -0300

    feat: refactor PlayStation scrapper logic and enhance price element processing

--- js/gaming/playstation.js
+++ js/gaming/playstation.js
@@ -3,260 +3,63 @@
  */
 function handlePlaystationMutations() {
   if (someURL(["playstation"], hostname)) {
-    console.log("🟢 impuestito is working...");
-    if (someURL(["/category", "/search"], pathname)) {
-      observeInit(document, handlePlaystationGrid);
-    }
-
-    if (someURL(["/pages", "/view"], pathname)) {
-      observeInit(document, handlePlaystationSlider);
-    }
-
-    if (someURL(["/games"], pathname)) {
-      observeInit(document, () => {
-        handlePlaystationGames();
-        handlePlaystationGamesEditions();
-      });
-    }
-
-    if (someURL(["/ps-plus"], pathname)) {
-      observeInit(document, handlePlaystationPlus);
-    }
-
-    if (someURL(["/product", "/concept"], pathname)) {
-      observeInit(document, () => {
-        handlePlaystationProduct();
-        handlePlaystationProductEditions();
-        handlePlaystationProductComplements();
-      });
-    }
+    writePlayground("PlayStation Store");
+    observeInit(document, PlaystationScrapper);
   }
 }
 
 /**
- * HOW IT WORKS?
+ * Scrapes PlayStation price elements from the document, processes them, and applies specific classes.
  *
- * Find a game item list
- * Then get the original price and their discount if exists
- * Then replace both with the new price
- * Then add a click event to switch between original and new prices
- */
-
-/*
+ * This function selects all `span` and `s` elements that contain a dollar sign (`$`) in their inner text,
+ * ensures the text length is less than 15 characters, and that the inner text matches the inner HTML.
+ * It then filters out elements that have already been scanned, adds specific classes to the remaining elements,
+ * and processes them if they haven't been processed yet.
+ *
+ * The processed elements are passed to the `scrapper` function with specific parameters, and a class indicating
+ * completion is added to each processed element.
+ *
  * Tested on:
  * https://store.playstation.com/es-ar/category/024029c7-f61b-4bef-a4d7-06270ed12b56
- */
-function handlePlaystationGrid() {
-  handleMutations(".psw-product-tile__details", "playstation--grid", (game) => {
-    // Price
-    scrapper({
-      priceElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      eventElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      currency: "US",
-      showEmoji: false,
-    });
-
-    // Regular Price
-    scrapper({
-      priceElement: game.lastElementChild.querySelector(".psw-c-t-2"),
-      eventElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      currency: "US",
-      showEmoji: false,
-      isDiscount: true,
-    });
-  });
-}
-
-/*
- * Tested on:
  * https://store.playstation.com/es-ar/pages/latest
  * https://store.playstation.com/es-ar/pages/collections
  * https://store.playstation.com/es-ar/pages/deals
- * https://store.playstation.com/es-ar/pages/ps5
  * https://store.playstation.com/es-ar/pages/subscriptions
- */
-function handlePlaystationSlider() {
-  handleMutations(".psw-strand-scroller .psw-product-tile__details", "playstation--slider", (game) => {
-    // Price
-    scrapper({
-      priceElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      eventElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      currency: "US",
-      showEmoji: false,
-    });
-
-    // Regular Price
-    scrapper({
-      priceElement: game.lastElementChild.querySelector(".psw-c-t-2"),
-      eventElement: game.lastElementChild.querySelector(".psw-m-r-3"),
-      currency: "US",
-      showEmoji: false,
-      isDiscount: true,
-    });
-  });
-}
-
-/*
- * Tested on:
- * https://www.playstation.com/es-ar/ps-plus
+ * https://www.playstation.com/es-ar/ps-plus/whats-new/
  * https://www.playstation.com/es-ar/ps-plus/this-month-on-ps-plus/
- */
-function handlePlaystationPlus() {
-  handleMutations(".tier-selector__tier", "playstation--plus", (game) => {
-    for (let offerNum = 0; offerNum < 3; offerNum++) {
-      // Price
-      scrapper({
-        priceElement: game.querySelector(`[data-qa="mfe-tier-selector#standalonePrice#offer${offerNum}#price"]`),
-        eventElement: game.querySelector(`[data-qa="mfe-tier-selector#standalonePrice#offer${offerNum}#price"]`),
-        currency: "US",
-        showEmoji: false,
-      });
-    }
-  });
-}
-
-/*
- * Tested on:
  * https://store.playstation.com/es-ar/product/UP1004-CUSA03041_00-RDR2ULTMEDTNBUND
  * https://store.playstation.com/es-ar/product/UP2047-CUSA01164_00-PLAGUEOFFROGSPAC
  * https://store.playstation.com/es-ar/concept/10000649
- */
-function handlePlaystationProduct() {
-  handleMutations(".pdp-cta", "playstation--product", (game) => {
-    for (let offerNum = 0; offerNum < 2; offerNum++) {
-      // Price
-      scrapper({
-        priceElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        eventElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        currency: "US",
-        showEmoji: false,
-      });
-
-      // Regular Price
-      scrapper({
-        priceElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#originalPrice"]`),
-        eventElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        currency: "US",
-        showEmoji: false,
-        isDiscount: true,
-      });
-    }
-  });
-}
-
-/*
- * Tested on:
- * https://store.playstation.com/es-ar/product/UP1004-CUSA03041_00-RDR2ULTMEDTNBUND
- */
-function handlePlaystationProductEditions() {
-  handleMutations(".pdp-upsells article", "playstation--product-editions", (game, i) => {
-    // Price
-    scrapper({
-      priceElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      eventElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      currency: "US",
-      showEmoji: false,
-    });
-
-    // Regular Price
-    scrapper({
-      priceElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#originalPrice"]`),
-      eventElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      currency: "US",
-      showEmoji: false,
-      isDiscount: true,
-    });
-  });
-}
-
-/*
- * Tested on:
- * https://store.playstation.com/es-ar/product/UP1004-CUSA03041_00-RDR2ULTMEDTNBUND
- */
-function handlePlaystationProductComplements() {
-  handleMutations('ul[data-qa="add-ons"] li', "playstation--product-complements", (game, i) => {
-    // Price
-    console.log(game);
-    scrapper({
-      priceElement: game.querySelector(`[data-qa="add-ons-grid#${i}#price#display-price"]`),
-      eventElement: game.querySelector(`[data-qa="add-ons-grid#${i}#price#display-price"]`),
-      currency: "US",
-      showEmoji: false,
-    });
-  });
-}
-
-/*
- * Tested on:
  * https://www.playstation.com/es-ar/games/god-of-war/
  * https://www.playstation.com/es-ar/games/horizon-zero-dawn/
- */
-function handlePlaystationGames() {
-  handleMutations(".game-hero__title-content", "playstation--games", (game) => {
-    for (let offerNum = 0; offerNum < 2; offerNum++) {
-      // Price
-      scrapper({
-        priceElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        eventElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        currency: "US",
-        showEmoji: false,
-      });
-
-      // Regular Price
-      scrapper({
-        priceElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#originalPrice"]`),
-        eventElement: game.querySelector(`[data-qa="mfeCtaMain#offer${offerNum}#finalPrice"]`),
-        currency: "US",
-        showEmoji: false,
-        isDiscount: true,
-      });
-    }
-  });
-}
-
-/*
- * Tested on:
  * https://www.playstation.com/es-ar/games/god-of-war/
  * https://www.playstation.com/es-ar/games/ea-sports-fifa-22/
+ *
+ * @function
  */
-function handlePlaystationGamesEditions() {
-  handleMutations('[data-qa="mfeUpsell"] article', "playstation--games-editions", (game, i) => {
-    // Price
-    scrapper({
-      priceElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      eventElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      currency: "US",
-      showEmoji: false,
+function PlaystationScrapper() {
+  const elements = [...document.querySelectorAll('span, s')]
+    .filter((e) => e.innerText.includes("$"))
+    .filter((e) => !alreadyScanned(e))
+    .map((e) => {
+      e.classList.add("impuestito", "impuestito-playstation")
+      return e;
     });
 
-    // Regular Price
-    scrapper({
-      priceElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#originalPrice"]`),
-      eventElement: game.querySelector(`[data-qa="mfeUpsell#productEdition${i}#ctaWithPrice#offer0#finalPrice"]`),
-      currency: "US",
-      showEmoji: false,
-      isDiscount: true,
-    });
-  });
+  const targetElements = elements.filter((e) => !alreadyProcessed(e));
+  if (targetElements.length > 0) {
+    for (const element of targetElements) {
+      scrapper({
+        priceElement: element,
+        eventElement: element,
+        currency: "US",
+        showEmoji: true,
+        isDiscount: false,
+      });
+      element.classList.add("impuestito-done");
+    }
+  }
 }
-
-/*
- * TODO: EA Play cards
- * Tested on:
- * https://store.playstation.com/es-ar/product/UP1004-CUSA03041_00-RDR2ULTMEDTNBUND
- */
-// function handlePlaystationEAPlay() {
-//   handleMutations("#gdk__content > div.section", (game, i) => {
-//     // Price
-//     // priceElement: [...document.querySelectorAll('#gdk__content div.section')[2].querySelectorAll('span[data-qa]')].filter(e=>e.innerText.includes('US$'))
-//     scrapper({
-//       priceElement: game.querySelectorAll("span").filter((e) => e.innerText.includes("$") && e.innerText.includes("Ahorra") && e.innerText === e.innerHTML),
-//       eventElement: game.querySelector(`[data-qa="add-ons-grid#${i}#price#display-price"]`),
-//       currency: "US",
-//       showEmoji: false,
-//     });
-//   });
-// }
 
 // Init
 handlePlaystationMutations();
